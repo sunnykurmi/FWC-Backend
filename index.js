1baf5fa2ec3492c304c178dbf502e1e21e595684
sunnykurmi <sunnykurmiskks@gmail.com>
2025-02-23 19:31:49 +0530

    updated cors

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
+      "https://fwc-india.org"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
