ab9f988601c49c7f1d5fab5fb5496689d816c8d7
adarsh0133 <adarsh90399@gmail.com>
2025-03-10 16:59:21 +0530

    course change

--- index.js
+++ index.js
@@ -25,11 +25,11 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  res.setHeader("Cross-Origin-Opener-Policy", "same-origin-allow-popups");
-  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Cross-Origin-Opener-Policy", "same-origin-allow-popups");
+//   res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+//   next();
+// });
 
 // body parser
 app.use(express.json());
