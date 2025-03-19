1d9d4582716085b533b56ad8aa01901c69d8c54f
adarsh0133 <adarsh90399@gmail.com>
2025-03-19 13:03:14 +0530

    some changes

--- src/routes/investmentCircle.routes.js
+++ src/routes/investmentCircle.routes.js
@@ -1,15 +1,20 @@
 let express = require("express");
-const { all_investment_circle, create_investment_circle } = require("../controllers/investmentCircle.controllers");
+const {
+  all_investment_circle,
+  create_investment_circle,
+} = require("../controllers/investmentCircle.controllers");
 const { isAuthenticated } = require("../middlewares/auth");
 const { isAdmin } = require("../middlewares/isAdmin");
 let router = express.Router();
 
 // create rout of events
-router.route("/all_investment_circle").post(isAuthenticated, isAdmin, all_investment_circle);
-
+router
+  .route("/all_investment_circle")
+  .post(isAuthenticated, isAdmin, all_investment_circle);
 
 // create rout of events
-router.route("/create_investment_circle").post(isAuthenticated,create_investment_circle);
-
+router
+  .route("/create_investment_circle")
+  .post(isAuthenticated, create_investment_circle);
 
 module.exports = router;
